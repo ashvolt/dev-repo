3e0a619212a0042838005d01c219bab6f1a5b4c8
OFS1\pragash.m <pragash.m@object-frontier.com>
2018-03-01 17:35:37 +0530

    needed transition

---
+++ project2/scripts/index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var element1 = document.getElementById("contact_extractor");
+    var element2 = document.getElementById("email_extractor");
+    element1.style.display="none";
+    element2.style.display="block"
+ }
